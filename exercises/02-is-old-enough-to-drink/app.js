054820e5d7689f23afc500b9b2ebdc7a67ba2b7a
Kevin Soria <Kevin.soria@outlook.com>
2020-05-06 03:02:46 +0000

    last touch before test

--- exercises/02-is-old-enough-to-drink/app.js
+++ exercises/02-is-old-enough-to-drink/app.js
@@ -3,6 +3,6 @@ function isOldEnoughToDrink(age) {
     if ( age >= 21 )
   { return true }
     else { return false }
-//   need to recheck test and make sure its working
+
 }
-console.log(isOldEnoughToDrink(22))6
+console.log(isOldEnoughToDrink(22))
